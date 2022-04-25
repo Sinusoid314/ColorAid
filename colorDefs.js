9de29ed5cdf4e312798990f09f28ed488cf653b4
CheesusChice <sinusoid314@gmail.com>
2022-04-25 17:56:17 -0400

    Add files via upload
    - Added index.html that redirects to coloraid.html.
    - Modified initCamera() in coloraid.js to request the rear-facing camera.
    - Reformatted source files.
    - Modified colorDef.js to use the 'class' syntax for Color and ColorRegion classes.
    - Added a version div to coloraid.html.

--- colorDefs.js
+++ colorDefs.js
@@ -10,142 +10,125 @@ const graySatMax = 10;
 const blackLumMax = 10;
 const whiteLumMin = 90;
 
-function Color(hue, sat, lum)
+class Color
 {
+  constructor(hue, sat, lum)
+  {
     this.hue = hue;
     this.sat = sat;
     this.lum = lum;
+  }
 
-    this.setFromRGB = function(red, green, blue)
-    //Convert the RGB components to the HSL components of this object
-    {
-        var cMin;
-        var cMax;
-        var delta;
+  setFromRGB(red, green, blue)
+  //Convert the RGB components to the HSL components of this object
+  {
+    var cMin;
+    var cMax;
+    var delta;
 
-        red /= 255;
-        green /= 255;
-        blue /= 255;
+    red /= 255;
+    green /= 255;
+    blue /= 255;
 
-        cMin = Math.min(red, green, blue);
-        cMax = Math.max(red, green, blue);
-        delta = cMax - cMin;
+    cMin = Math.min(red, green, blue);
+    cMax = Math.max(red, green, blue);
+    delta = cMax - cMin;
 
-        //Calculate hue
-        if(delta == 0)
-        {
-            this.hue = 0;
-	    }
-	    else if(cMax == red)
-	    {
-            this.hue = ((green - blue) / delta) % 6;
-	    }
-	    else if(cMax == green)
-	    {
-            this.hue = ((blue - red) / delta) + 2;
-	    }
-	    else if(cMax == blue)
-	    {
-            this.hue = ((red - green) / delta) + 4;
-	    }
+    //Calculate hue
+    if(delta == 0)
+      this.hue = 0;
+    else if(cMax == red)
+      this.hue = ((green - blue) / delta) % 6;
+    else if(cMax == green)
+      this.hue = ((blue - red) / delta) + 2;
+    else if(cMax == blue)
+      this.hue = ((red - green) / delta) + 4;
 
-        //Convert hue to degrees
-        this.hue = Math.round(60 * this.hue);
-	    if(this.hue < 0)
-	    {
-			this.hue += 360;
-	    }
+    //Convert hue to degrees
+    this.hue = Math.round(60 * this.hue);
+    if(this.hue < 0)
+      this.hue += 360;
 
-        //Calculate luminance
-        this.lum = (cMax + cMin) / 2;
+    //Calculate luminance
+    this.lum = (cMax + cMin) / 2;
 
-        //Calculate saturation
-        if(delta == 0)
-        {
-            this.sat = 0;
-		}
-		else
-		{
-            this.sat = delta / (1 - Math.abs(2 * this.lum - 1));
-		}
+    //Calculate saturation
+    if(delta == 0)
+      this.sat = 0;
+    else
+      this.sat = delta / (1 - Math.abs(2 * this.lum - 1));
 
-		//Convert luminance and saturation to percentages
-		this.sat = Math.round(this.sat * 100);
-		this.lum = Math.round(this.lum * 100);
-    };
+    //Convert luminance and saturation to percentages
+    this.sat = Math.round(this.sat * 100);
+    this.lum = Math.round(this.lum * 100);
+  }
 }
 
-function ColorRegion(name, min, max)
+class ColorRegion
 {
+  constructor(name, min, max)
+  {
     this.name = name;
     this.min = min;
     this.max = max;
+  }
 
-    this.containsColor = function(color)
-    //Returns true if the given color is within this region, otherwise false
+  containsColor(color)
+  //Returns true if the given color is within this region, otherwise false
+  {
+    //Check along the hue axis
+    if(this.max.hue < this.min.hue)
     {
-		//Check along the hue axis
-        if(this.max.hue < this.min.hue)
-        {
-		    //This deals with a region that wraps around the 0/360 degree hue boundary
-            if((color.hue < this.min.hue) && (color.hue > this.max.hue))
-            {
-			    return false;
-		    }
-	    }
-	    else
-	    {
-		    if((color.hue < this.min.hue) || (color.hue > this.max.hue))
-		    {
-			    return false;
-		    }
-		}
+      //This deals with a region that wraps around the 0/360 degree hue boundary
+      if((color.hue < this.min.hue) && (color.hue > this.max.hue))
+        return false;
+    }
+    else
+    {
+      if((color.hue < this.min.hue) || (color.hue > this.max.hue))
+        return false;
+    }
 
-		//Check along the saturation axis
-        if((color.sat < this.min.sat) || (color.sat > this.max.sat))
-		{
-            return false;
-        }
+    //Check along the saturation axis
+    if((color.sat < this.min.sat) || (color.sat > this.max.sat))
+      return false;
 
-        //Check along the luminance axis
-        if((color.lum < this.min.lum) || (color.lum > this.max.lum))
-		{
-            return false;
-        }
+    //Check along the luminance axis
+    if((color.lum < this.min.lum) || (color.lum > this.max.lum))
+      return false;
 
-        return true;
-    };
+    return true;
+  }
 }
 
-var colorRegionList =
-    [
-    new ColorRegion("Orange", new Color(orangeHueMin, graySatMax + 1, blackLumMax + 1),
-                              new Color(yellowHueMin - 1, 100, whiteLumMin - 1)),
+var colorRegionList = [
+        new ColorRegion("Orange", new Color(orangeHueMin, graySatMax + 1, blackLumMax + 1),
+                                  new Color(yellowHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Yellow", new Color(yellowHueMin, graySatMax + 1, blackLumMax + 1),
-                              new Color(greenHueMin - 1, 100, whiteLumMin - 1)),
+        new ColorRegion("Yellow", new Color(yellowHueMin, graySatMax + 1, blackLumMax + 1),
+                                  new Color(greenHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Green", new Color(greenHueMin, graySatMax + 1, blackLumMax + 1),
-                             new Color(tealHueMin - 1, 100, whiteLumMin - 1)),
+        new ColorRegion("Green", new Color(greenHueMin, graySatMax + 1, blackLumMax + 1),
+                                 new Color(tealHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Teal", new Color(tealHueMin, graySatMax + 1, blackLumMax + 1),
-                            new Color(blueHueMin - 1, 100, whiteLumMin - 1)),
+        new ColorRegion("Teal", new Color(tealHueMin, graySatMax + 1, blackLumMax + 1),
+                                new Color(blueHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Blue", new Color(blueHueMin, graySatMax + 1, blackLumMax + 1),
-                            new Color(purpleHueMin - 1, 100, whiteLumMin - 1)),
+        new ColorRegion("Blue", new Color(blueHueMin, graySatMax + 1, blackLumMax + 1),
+                                new Color(purpleHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Purple", new Color(purpleHueMin, graySatMax + 1, blackLumMax + 1),
-                              new Color(redHueMin - 1, 100, whiteLumMin - 1)),
+        new ColorRegion("Purple", new Color(purpleHueMin, graySatMax + 1, blackLumMax + 1),
+                                  new Color(redHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Red", new Color(redHueMin, graySatMax + 1, blackLumMax + 1),
-                           new Color(orangeHueMin - 1, 100, whiteLumMin - 1)),
+        new ColorRegion("Red", new Color(redHueMin, graySatMax + 1, blackLumMax + 1),
+                               new Color(orangeHueMin - 1, 100, whiteLumMin - 1)),
 
-    new ColorRegion("Gray", new Color(0, 0, blackLumMax + 1),
-                            new Color(360, graySatMax, whiteLumMin - 1)),
+        new ColorRegion("Gray", new Color(0, 0, blackLumMax + 1),
+                                new Color(360, graySatMax, whiteLumMin - 1)),
 
-    new ColorRegion("Black", new Color(0, 0, 0),
-                             new Color(360, 100, blackLumMax)),
+        new ColorRegion("Black", new Color(0, 0, 0),
+                                 new Color(360, 100, blackLumMax)),
 
-    new ColorRegion("White", new Color(0, 0, whiteLumMin),
-                             new Color(360, 100, 100))
-    ];
+        new ColorRegion("White", new Color(0, 0, whiteLumMin),
+                                 new Color(360, 100, 100))
+   ];
--- coloraid.js
+++ coloraid.js
@@ -11,98 +11,98 @@ setEvents();
 
 function initCamera()
 {
-    if('mediaDevices' in navigator)
+  if('mediaDevices' in navigator)
+  {
+    navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}})
+    .then((mediaStream) =>
     {
-        navigator.mediaDevices.getUserMedia({ video: true })
-        .then((mediaStream) =>
-              {
-                  cameraView.srcObject = mediaStream;
-                  cameraView.play();
-                  return mediaStream;
-              })
-		.catch((err) =>
-               {
-                   alert("Unable to access camera: " + err);
-               });
-    }
-    else
+      cameraView.srcObject = mediaStream;
+      cameraView.play();
+      return mediaStream;
+    })
+    .catch((err) =>
     {
-        alert("Your browser does not support media devices.");
-    }
+      alert("Unable to access camera: " + err);
+    });
+  }
+  else
+  {
+    alert("Your browser does not support media devices.");
+  }
 }
 
 function setEvents()
 {
-    cameraBtn.addEventListener("click", cameraBtn_OnClick);
-    cameraBtn.addEventListener("tap", cameraBtn_OnClick);
-    snapshotBtn.addEventListener("click", snapshotBtn_OnClick);
-    snapshotBtn.addEventListener("tap", snapshotBtn_OnClick);
-    cameraView.addEventListener("click", cameraView_OnClick);
-    cameraView.addEventListener("tap", cameraView_OnClick);
-    snapshotView.addEventListener("click", snapshotView_OnClick);
-    snapshotView.addEventListener("tap", snapshotView_OnClick);
+  cameraBtn.addEventListener("click", cameraBtn_OnClick);
+  cameraBtn.addEventListener("tap", cameraBtn_OnClick);
+  snapshotBtn.addEventListener("click", snapshotBtn_OnClick);
+  snapshotBtn.addEventListener("tap", snapshotBtn_OnClick);
+  cameraView.addEventListener("click", cameraView_OnClick);
+  cameraView.addEventListener("tap", cameraView_OnClick);
+  snapshotView.addEventListener("click", snapshotView_OnClick);
+  snapshotView.addEventListener("tap", snapshotView_OnClick);
 }
 
 function cameraBtn_OnClick(eventObj)
 //Switch to camera view
 {
-    cameraView.style.display = "block";
-	snapshotView.style.display = "none";
-    matchView.style.display = "none";
+  cameraView.style.display = "block";
+  snapshotView.style.display = "none";
+  matchView.style.display = "none";
 
-	cameraBtn.className = "highlight";
-	snapshotBtn.className = "unhighlight";
+  cameraBtn.className = "highlight";
+  snapshotBtn.className = "unhighlight";
 
-	cameraText.style.display = "block";
-	snapshotText.style.display = "none";
+  cameraText.style.display = "block";
+  snapshotText.style.display = "none";
 }
 
 function snapshotBtn_OnClick(eventObj)
 //Switch to snapshot view
 {
-    snapshotView.style.display = "block";
-	cameraView.style.display = "none";
+  snapshotView.style.display = "block";
+  cameraView.style.display = "none";
 
-	snapshotBtn.className = "highlight";
-	cameraBtn.className = "unhighlight";
+  snapshotBtn.className = "highlight";
+  cameraBtn.className = "unhighlight";
 
-	snapshotText.style.display = "block";
-	cameraText.style.display = "none";
+  snapshotText.style.display = "block";
+  cameraText.style.display = "none";
 }
 
 function cameraView_OnClick(eventObj)
 //Capture snapshot from camera
 {
-    snapshotContext.drawImage(cameraView, 0, 0, snapshotView.width, snapshotView.height);
-    snapshotBtn_OnClick();
+  snapshotContext.drawImage(cameraView, 0, 0, snapshotView.width, snapshotView.height);
+  snapshotBtn_OnClick();
 }
 
 function snapshotView_OnClick(eventObj)
 //Find the closest matching color to the selected snapshot pixel
 {
-    var snapshotViewRect;
-    var sampleX;
-    var sampleY;
-    var rgbData;
-    var sampleColor = new Color(0, 0, 0);
-    var matchRegion;
+  var snapshotViewRect;
+  var sampleX;
+  var sampleY;
+  var rgbData;
+  var sampleColor = new Color(0, 0, 0);
+  var matchRegion;
 
-    //Set sampleX/Y to the event click-point, relative to snapshotView
-    snapshotViewRect = snapshotView.getBoundingClientRect();
-    sampleX = eventObj.clientX - snapshotViewRect.left;
-    sampleY = eventObj.clientY - snapshotViewRect.top;
+  //Set sampleX/Y to the event click-point, relative to snapshotView
+  snapshotViewRect = snapshotView.getBoundingClientRect();
+  sampleX = eventObj.clientX - snapshotViewRect.left;
+  sampleY = eventObj.clientY - snapshotViewRect.top;
 
-    //Get the HSL color at sampleX/Y
-    rgbData = snapshotContext.getImageData(sampleX, sampleY, 1, 1).data;
-    sampleColor.setFromRGB(rgbData[0], rgbData[1], rgbData[2]);
+  //Get the HSL color at sampleX/Y
+  rgbData = snapshotContext.getImageData(sampleX, sampleY, 1, 1).data;
+  sampleColor.setFromRGB(rgbData[0], rgbData[1], rgbData[2]);
 
-    //Find the color region that sampleColor is in
-    matchRegion = colorRegionList.find(item => item.containsColor(sampleColor));
+  //Find the color region that sampleColor is in
+  matchRegion = colorRegionList.find(item => item.containsColor(sampleColor));
 
-    //Display the name of the matching color region at the click-point
-    matchView.innerHTML = matchRegion.name;
-    matchView.style.left = eventObj.pageX;
-    matchView.style.top = eventObj.pageY;
-    matchView.style.display = "block";
+  //Display the name of the matching color region at the click-point
+  matchView.innerHTML = matchRegion.name;
+  matchView.style.left = eventObj.pageX;
+  matchView.style.top = eventObj.pageY;
+  matchView.style.display = "block";
 }
 
